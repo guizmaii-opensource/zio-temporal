0d1ac3a374404fc5979786abae05c09dfd0ed88b
Jules Ivanic <jules.ivanic@gmail.com>
2025-06-10 02:00:23 +0000

    Update micrometer-registry-otlp to 1.15.1

--- project/BuildConfig.scala
+++ project/BuildConfig.scala
@@ -145,6 +145,6 @@ trait Dependencies {
 
     val otel = Seq(otelApi, otelExporterOtlp, otelTracePropagators, otelOpentracingShim, otelSemvonc)
 
-    val micrometerOtlp = "io.micrometer" % "micrometer-registry-otlp" % "1.15.0"
+    val micrometerOtlp = "io.micrometer" % "micrometer-registry-otlp" % "1.15.1"
   }
 }
