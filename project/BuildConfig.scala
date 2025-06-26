3c571f784b5d71985847d565dc56cc5a4b8e0834
Jules Ivanic <jules.ivanic@gmail.com>
2025-06-26 01:59:40 +0000

    Update temporal-opentracing, temporal-sdk, ... to 1.30.1

--- project/BuildConfig.scala
+++ project/BuildConfig.scala
@@ -68,7 +68,7 @@ object BuildConfig extends Dependencies {
 trait Dependencies {
 
   private object versions {
-    val temporal   = "1.29.0"
+    val temporal   = "1.30.1"
     val zio        = "2.1.19"
     val zioLogging = "2.5.0"
     val enumeratum = "1.9.0"
