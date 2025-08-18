b8746d2cb6896ab90d999928089d5ab30e324f38
Jules Ivanic <jules.ivanic@gmail.com>
2025-08-18 12:49:41 +1000

    Update micrometer-registry-otlp to 1.15.3 (#88)
    ## About this PR
    📦 Updates
    [io.micrometer:micrometer-registry-otlp](https://github.com/micrometer-metrics/micrometer)
    from `1.15.2` to `1.15.3`
    
    📜 [GitHub Release
    Notes](https://github.com/micrometer-metrics/micrometer/releases/tag/v1.15.3)
    - [Version
    Diff](https://github.com/micrometer-metrics/micrometer/compare/v1.15.2...v1.15.3)
    
    ## Usage
    ✅ **Please merge!**
    
    I'll automatically update this PR to resolve conflicts as long as you
    don't change it yourself.
    
    If you'd like to skip this version, you can just close this PR. If you
    have any feedback, just mention me in the comments below.
    
    Configure Scala Steward for your repository with a
    [`.scala-steward.conf`](https://github.com/scala-steward-org/scala-steward/blob/0c7991de8224eff8436230a3e11b89520bbf94c8/docs/repo-specific-configuration.md)
    file.
    
    _Have a fantastic day writing Scala!_
    
    <details>
    <summary>⚙ Adjust future updates</summary>
    
    Add this to your `.scala-steward.conf` file to ignore future updates of
    this dependency:
    ```
    updates.ignore = [ { groupId = "io.micrometer", artifactId = "micrometer-registry-otlp" } ]
    ```
    Or, add this to slow down future updates of this dependency:
    ```
    dependencyOverrides = [{
      pullRequests = { frequency = "30 days" },
      dependency = { groupId = "io.micrometer", artifactId = "micrometer-registry-otlp" }
    }]
    ```
    </details>
    
    <sup>
    labels: library-update, early-semver-patch, semver-spec-patch,
    commit-count:1
    </sup>
    
    <!-- scala-steward = {
      "Update" : {
        "ForArtifactId" : {
          "crossDependency" : [
            {
              "groupId" : "io.micrometer",
              "artifactId" : {
                "name" : "micrometer-registry-otlp",
                "maybeCrossName" : null
              },
              "version" : "1.15.2",
              "sbtVersion" : null,
              "scalaVersion" : null,
              "configurations" : null
            }
          ],
          "newerVersions" : [
            "1.15.3"
          ],
          "newerGroupId" : null,
          "newerArtifactId" : null
        }
      },
      "Labels" : [
        "library-update",
        "early-semver-patch",
        "semver-spec-patch",
        "commit-count:1"
      ]
    } -->

--- project/BuildConfig.scala
+++ project/BuildConfig.scala
@@ -145,6 +145,6 @@ trait Dependencies {
 
     val otel = Seq(otelApi, otelExporterOtlp, otelTracePropagators, otelOpentracingShim, otelSemvonc)
 
-    val micrometerOtlp = "io.micrometer" % "micrometer-registry-otlp" % "1.15.2"
+    val micrometerOtlp = "io.micrometer" % "micrometer-registry-otlp" % "1.15.3"
   }
 }
