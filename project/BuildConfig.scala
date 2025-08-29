dcf3db8feb2cca2bec240c5ca24d20bc1f93e894
Jules Ivanic <jules.ivanic@gmail.com>
2025-08-29 01:49:28 +0000

    Update jackson-module-scala to 2.20.0

--- project/BuildConfig.scala
+++ project/BuildConfig.scala
@@ -72,7 +72,7 @@ trait Dependencies {
     val zio        = "2.1.20"
     val zioLogging = "2.5.1"
     val enumeratum = "1.9.0"
-    val jackson    = "2.19.2"
+    val jackson    = "2.20.0"
     val otel       = "1.53.0"
   }
 
