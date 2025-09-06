101a64822f188f7174a5aca7b5b2c4b8493b2798
Jules Ivanic <jules.ivanic@gmail.com>
2025-09-06 01:43:36 +0000

    Update opentelemetry-api, ... to 1.54.0

--- project/BuildConfig.scala
+++ project/BuildConfig.scala
@@ -73,7 +73,7 @@ trait Dependencies {
     val zioLogging = "2.5.1"
     val enumeratum = "1.9.0"
     val jackson    = "2.20.0"
-    val otel       = "1.53.0"
+    val otel       = "1.54.0"
   }
 
   object org {
