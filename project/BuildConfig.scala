e59f28bde0768e71734500b60c61165f6584ba48
Jules Ivanic <jules.ivanic@gmail.com>
2026-02-27 11:33:00 +1100

    Update zio-cli to 0.8.0 (#183)
    ## About this PR
    📦 Updates [dev.zio:zio-cli](https://github.com/zio/zio-cli/) from
    `0.7.5` to `0.8.0`
    
    📜 [GitHub Release
    Notes](https://github.com/zio/zio-cli/releases/tag/v0.8.0) - [Version
    Diff](https://github.com/zio/zio-cli/compare/v0.7.5...v0.8.0)
    
    ## Usage
    ✅ **Please merge!**
    
    I'll automatically update this PR to resolve conflicts as long as you
    don't change it yourself.
    
    If you'd like to skip this version, you can just close this PR. If you
    have any feedback, just mention me in the comments below.
    
    Configure Scala Steward for your repository with a
    [`.scala-steward.conf`](https://github.com/scala-steward-org/scala-steward/blob/eaf4ea2f476b6b4988670d6cb6d9f9355e58780e/docs/repo-specific-configuration.md)
    file.
    
    _Have a fantastic day writing Scala!_
    
    <details>
    <summary>⚙ Adjust future updates</summary>
    
    Add this to your `.scala-steward.conf` file to ignore future updates of
    this dependency:
    ```
    updates.ignore = [ { groupId = "dev.zio", artifactId = "zio-cli" } ]
    ```
    Or, add this to slow down future updates of this dependency:
    ```
    dependencyOverrides = [{
      pullRequests = { frequency = "30 days" },
      dependency = { groupId = "dev.zio", artifactId = "zio-cli" }
    }]
    ```
    </details>
    
    <sup>
    labels: library-update, early-semver-major, semver-spec-minor,
    commit-count:1
    </sup>
    
    <!-- scala-steward = {
      "Update" : {
        "ForArtifactId" : {
          "crossDependency" : [
            {
              "groupId" : "dev.zio",
              "artifactId" : {
                "name" : "zio-cli",
                "maybeCrossName" : "zio-cli_3"
              },
              "version" : "0.7.5",
              "sbtVersion" : null,
              "scalaVersion" : null,
              "configurations" : null
            }
          ],
          "newerVersions" : [
            "0.8.0"
          ],
          "newerGroupId" : null,
          "newerArtifactId" : null
        }
      },
      "Labels" : [
        "library-update",
        "early-semver-major",
        "semver-spec-minor",
        "commit-count:1"
      ]
    } -->

--- project/BuildConfig.scala
+++ project/BuildConfig.scala
@@ -84,7 +84,7 @@ trait Dependencies {
     val testFrameworks = Seq(new TestFramework("zio.test.sbt.ZTestFramework"))
 
     // only for examples
-    val cli = "dev.zio" %% "zio-cli" % "0.7.5"
+    val cli = "dev.zio" %% "zio-cli" % "0.8.0"
   }
 
   object Scalapb {
