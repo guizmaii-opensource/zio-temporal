2daa712a9973f9df8cea8ca85001ce66919dbd56
Jules Ivanic <jules.ivanic@gmail.com>
2026-05-01 15:48:00 +1000

    Update temporal-opentracing, temporal-sdk, ... to 1.35.0 (#209)
    ## About this PR
    📦 Updates 
    *
    [io.temporal:temporal-opentracing](https://github.com/temporalio/sdk-java)
    * [io.temporal:temporal-sdk](https://github.com/temporalio/sdk-java)
    * [io.temporal:temporal-testing](https://github.com/temporalio/sdk-java)
    
     from `1.34.0` to `1.35.0`
    
    📜 [GitHub Release
    Notes](https://github.com/temporalio/sdk-java/releases/tag/v1.35.0) -
    [Version
    Diff](https://github.com/temporalio/sdk-java/compare/v1.34.0...v1.35.0)
    
    ## Usage
    ✅ **Please merge!**
    
    I'll automatically update this PR to resolve conflicts as long as you
    don't change it yourself.
    
    If you'd like to skip this version, you can just close this PR. If you
    have any feedback, just mention me in the comments below.
    
    Configure Scala Steward for your repository with a
    [`.scala-steward.conf`](https://github.com/scala-steward-org/scala-steward/blob/cfc35f4e63bef9d4423f375b1fef35caf642e61a/docs/repo-specific-configuration.md)
    file.
    
    _Have a fantastic day writing Scala!_
    
    <details>
    <summary>⚙ Adjust future updates</summary>
    
    Add this to your `.scala-steward.conf` file to ignore future updates of
    this dependency:
    ```
    updates.ignore = [ { groupId = "io.temporal" } ]
    ```
    Or, add this to slow down future updates of this dependency:
    ```
    dependencyOverrides = [{
      pullRequests = { frequency = "30 days" },
      dependency = { groupId = "io.temporal" }
    }]
    ```
    </details>
    
    <sup>
    labels: library-update, early-semver-minor, semver-spec-minor,
    commit-count:1
    </sup>
    
    <!-- scala-steward = {
      "Update" : {
        "ForGroupId" : {
          "forArtifactIds" : [
            {
              "ForArtifactId" : {
                "crossDependency" : [
                  {
                    "groupId" : "io.temporal",
                    "artifactId" : {
                      "name" : "temporal-opentracing",
                      "maybeCrossName" : null
                    },
                    "version" : "1.34.0",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : null
                  },
                  {
                    "groupId" : "io.temporal",
                    "artifactId" : {
                      "name" : "temporal-opentracing",
                      "maybeCrossName" : null
                    },
                    "version" : "1.34.0",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : "optional"
                  }
                ],
                "newerVersions" : [
                  "1.35.0"
                ],
                "newerGroupId" : null,
                "newerArtifactId" : null
              }
            },
            {
              "ForArtifactId" : {
                "crossDependency" : [
                  {
                    "groupId" : "io.temporal",
                    "artifactId" : {
                      "name" : "temporal-sdk",
                      "maybeCrossName" : null
                    },
                    "version" : "1.34.0",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : null
                  }
                ],
                "newerVersions" : [
                  "1.35.0"
                ],
                "newerGroupId" : null,
                "newerArtifactId" : null
              }
            },
            {
              "ForArtifactId" : {
                "crossDependency" : [
                  {
                    "groupId" : "io.temporal",
                    "artifactId" : {
                      "name" : "temporal-testing",
                      "maybeCrossName" : null
                    },
                    "version" : "1.34.0",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : null
                  }
                ],
                "newerVersions" : [
                  "1.35.0"
                ],
                "newerGroupId" : null,
                "newerArtifactId" : null
              }
            }
          ]
        }
      },
      "Labels" : [
        "library-update",
        "early-semver-minor",
        "semver-spec-minor",
        "commit-count:1"
      ]
    } -->

--- project/BuildConfig.scala
+++ project/BuildConfig.scala
@@ -53,7 +53,7 @@ object BuildConfig extends Dependencies {
 trait Dependencies {
 
   private object versions {
-    val temporal   = "1.34.0"
+    val temporal   = "1.35.0"
     val zio        = "2.1.25"
     val zioLogging = "2.5.3"
     val zioPrelude = "1.0.0-RC47"
